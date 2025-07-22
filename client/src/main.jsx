76244cfcdd468836694b96469f8038f78084f29c
ChamaraJanith <chamaragithub21@gmail.com>
2025-07-22 17:26:33 +0530

    basic created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,13 @@
-import React, { StrictMode } from 'react'
+import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { Provider } from './components/ui/provider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 )
