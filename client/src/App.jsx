80ade3a6fcee63c2e9ad715926282823ea3da2a5
ChamaraJanith <chamaragithub21@gmail.com>
2025-07-23 10:04:24 +0530

    chatbot done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,26 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import CreatePage from "./pages/CreatePage.jsx"
 import Navbar from "./component/Navbar.jsx"   
+import Chatbot from "./AI/Chatbot.jsx"
 
 const Demo = () => {
   return (
     <Box minH={"100vh"}>
       {/* Add the Navbar here */}
       <Navbar />
+
+      <div>
+        <Chatbot/>
+      </div>
       
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<CreatePage />} />
       </Routes>
     </Box>
+
+    
   )
+
 }
 export default Demo
