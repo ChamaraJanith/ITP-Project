a108596f014bae13df2e4c5a865e71cd6bf4ba63
ChamaraJanith <chamaragithub21@gmail.com>
2025-07-28 20:48:58 +0530

    feat: update environment variables and improve MongoDB connection handling

--- server/model/userModel.js
+++ server/model/userModel.js
@@ -1,49 +1,16 @@
-import { verify } from "jsonwebtoken";
+// ./model/userModel.js
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true   // Optional, but usually good for emails
-    },
-    password:{
-        type: String,
-        required: true,
-    },
-    verifyOtp:{
-        type:String,
-        default:''
-    },
-
-    verifyOtpExpireAt:{
-        type:Number,
-        default:0
-    },
-    isAccountVerified:
-    {
-        type:Boolean,
-        default:false
-    },
-    resetOtp:
-    { 
-        type:Boolean,
-        default:''
-    },
-    resetOtpExpireAt:
-    {
-        type:Number,
-        default:0
-    },
-
-    
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  verifyOtp: { type: String, default: '' },
+  verifyOtpExpireAt: { type: Number, default: 0 },
+  isAccountVerified: { type: Boolean, default: false },
+  resetOtp: { type: String, default: '' },
+  resetOtpExpireAt: { type: Number, default: 0 },
 });
 
-// Create the User model from the schema
-const userModel = mongoose.models.user || mongoose.model('user', userSchema);
-
-export default userModel;
+const UserModel = mongoose.models.User || mongoose.model('User', userSchema);
+export default UserModel;
