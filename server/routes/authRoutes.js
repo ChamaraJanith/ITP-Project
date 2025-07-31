b3382cac72a36a3d7a487ae584bda0c53dba4a15
ChamaraJanith <chamaragithub21@gmail.com>
2025-07-31 12:47:08 +0530

    feat: implement password reset functionality and enhance authentication routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { register, login, logout, sendVerifyOtp, verifyEmail} from '../controller/authController.js';
+import { register, login, logout, sendVerifyOtp, verifyEmail, isAuthenticated} from '../controller/authController.js';
 import userAuth from '../middleware/userAuth.js';
 
 
@@ -13,5 +13,6 @@ authRouter.post('/login', login);
 authRouter.post('/logout', logout);
 authRouter.post('/sendverifyotp', userAuth,sendVerifyOtp);
 authRouter.post('/verifyaccount', userAuth,verifyEmail);
+authRouter.post('/isAuth', userAuth,isAuthenticated);
 
  export default authRouter;
